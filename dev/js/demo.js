038e7f3377527102d56dffd4689f97164f53180a
alexhotchkiss <63318668+alexhotchkiss@users.noreply.github.com>
2020-09-27 14:00:41 -0400

    demo done

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,4 +1,6 @@
 import {gsap} from "gsap";
 
-gsap.to(".triangle-red",{duration: 2, alpha:0.5, rotation:180, x:500, y:200})
-gsap.to(".triangle-blue",{duration: 2, alpha:0.5, rotation:90, x:500})
+gsap.to(".triangle-red",{duration: 2, alpha:0.5, rotation:180, x:500, y:200});
+gsap.to(".triangle-blue",{duration: 2, alpha:0.5, rotation:90, x:500});
+gsap.to(".triangle-green",{duration: 2, alpha:0.5, rotation:-90, x:500, y:-200});
+gsap.to(".triangle-black",{duration: 2, alpha:0.5, rotation:-180, x:500, y:-200})
