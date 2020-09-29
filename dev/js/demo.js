6b9b79bcffad92e58ee0e4f231401bb5b4e996e0
alexhotchkiss <63318668+alexhotchkiss@users.noreply.github.com>
2020-09-29 15:45:31 -0400

    Update demo.js

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,5 +1,7 @@
 import {gsap} from "gsap";
 
+var animation = gsap.timeline() 
+
 gsap.to(".triangle-red",{duration: 2, alpha:0.5, rotation:180, x:500, y:200});
 gsap.to(".triangle-blue",{duration: 2, alpha:0.5, rotation:90, x:500});
 gsap.to(".triangle-green",{duration: 2, alpha:0.5, rotation:-90, x:500, y:-200});
